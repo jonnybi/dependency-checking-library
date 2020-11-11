4c30730a165b3768b6290cfd7b0a97db2f55609d
Johannes Bickel <johannes.bickel@leica-geosystems.com>
2020-11-11 18:52:36 +0100

    added deserialization feature

--- MyBaseLibrary/IMyToolClass.cs
+++ MyBaseLibrary/IMyToolClass.cs
@@ -3,5 +3,7 @@
     public interface IMyToolClass
     {
         string Serialize<T>(T obj);
+
+        T Deserialize<T>(string json);
     }
 }
--- MyBaseLibrary/MyToolClass.cs
+++ MyBaseLibrary/MyToolClass.cs
@@ -4,6 +4,11 @@ namespace MyBaseLibrary
 {
     public class MyToolClass : IMyToolClass
     {
+        public T Deserialize<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
         public string Serialize<T>(T obj)
         {
             return JsonConvert.SerializeObject(obj);
